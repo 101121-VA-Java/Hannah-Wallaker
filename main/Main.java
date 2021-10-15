d466825892393b6f133a882df133062a434cebcf
hannahwallaker <hanwallaker@gmail.com>
2021-10-15 12:50:05 -0400

    Adding reorganized folders and updating

---
+++ main/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main (String[] args){
+        //auto generated method here
+
+    System.out.println("Fashion-forward Kanye West is ");
+        Shirt s = new Shirt("It is Polka Dotted", 'S');
+        s.wear();
+
+    
+
+        System.out.println(s.color);
+        System.out.println(s.size);
+
+
+    }
+}
---
+++ shirt/Shirt.java
@@ -0,0 +1,34 @@
+public class Shirt{
+
+public static String color;
+public static char size;
+
+
+Shirt(String newColor, char newSize ){
+    color = newColor;
+    size = newSize;
+}
+
+
+Shirt() {
+}
+
+    public static void wear() {
+        System.out.println("wearing the shirt.");
+
+    }
+
+    public static void notwear() {
+        System.out.println("shirtless");
+    }
+    
+    public static void setColor(String newColor){
+        color = newColor;
+    }
+
+    public static void setSize(char newSize){
+        size = newSize;
+    }
+
+
+}
